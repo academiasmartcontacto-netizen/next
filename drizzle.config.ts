90c5f742ce8463b0718aa484c0756c38df240e43
qmcpre-maker <qmcpre@gmail.com>
2026-03-11 23:40:47 -0400

    refactor: use env variables for db connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,16 @@
+import 'dotenv/config'
 import type { Config } from 'drizzle-kit'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set in .env file')
+}
 
 export default {
   schema: './lib/db/schema.ts',
   out: './lib/db/migrations',
   driver: 'pg',
   dbCredentials: {
-    connectionString: 'postgresql://postgres.sfbsplymrielpfkoalsd:Yhefri123Chipana@aws-1-sa-east-1.pooler.supabase.com:6543/postgres',
+    connectionString: process.env.DATABASE_URL,
   },
   verbose: true,
   strict: true,
