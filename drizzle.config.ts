de405160913a400e1400cc9f021b66c87244e228
qmcpre-maker <qmcpre@gmail.com>
2026-03-12 00:45:37 -0400

    fix(dev): configure drizzle studio to run correctly

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,12 @@
-import 'dotenv/config'
-import type { Config } from 'drizzle-kit'
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set in .env file')
-}
-
-export default {
+export default defineConfig({
   schema: './lib/db/schema.ts',
-  out: './lib/db/migrations',
-  driver: 'pg',
+  out: './drizzle',
+  dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
+    ssl: 'require',
   },
-  verbose: true,
-  strict: true,
-} satisfies Config
+});
